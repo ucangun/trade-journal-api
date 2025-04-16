4f7ffa64063eca5098ca00716d9dd192b0a7109e
ucangun <149247682+ucangun@users.noreply.github.com>
2025-04-16 15:13:51 +0200

    feat: Add database connection and update npm scripts

---
+++ src/configs/dbConnection.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/* ------------------------------------------------- */
+/*                 TRADE JOURNAL API                 */
+/* ------------------------------------------------- */
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connection successful!");
+  } catch (err) {
+    console.error(`DB connection failed: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
